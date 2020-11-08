439be2188b1fd0e9c490d84d4b0e598384201b20
Emil <khisma017@gmail.com>
2020-11-08 14:08:44 +0100

    Дорабатывает функционал фильтрации в соответствии с комментариями наставника

--- js/filter.js
+++ js/filter.js
@@ -13,17 +13,33 @@
       const filteredPins = [];
 
       for (let i = 0; i < pins.length; i += 1) {
-        const housingTypeFilterConditions = elements.housingTypeFilter.value === `any` || pins[i].offer.type === elements.housingTypeFilter.value;
-        const priceFilterConditions = elements.priceFilter.value === `any` || (elements.priceFilter.value === `low` && pins[i].offer.price <= PRICE_FILTER_LOW) || (elements.priceFilter.value === `middle` && pins[i].offer.price > PRICE_FILTER_LOW && pins[i].offer.price <= PRICE_FILTER_MIDDLE) || (elements.priceFilter.value === `high` && pins[i].offer.price >= PRICE_FILTER_MIDDLE);
-        const roomsNumberConditions = elements.roomsNumberFilter.value === `any` || pins[i].offer.rooms === Number(elements.roomsNumberFilter.value);
-        const guestsNumberConditions = elements.guestsNumberFilter.value === `any` || pins[i].offer.guests === Number(elements.guestsNumberFilter.value);
-        const isChecked = (input) => !input.checked || (input.checked && pins[i].offer.features.includes(input.value));
-        const featuresConditions = featuresInputsArray.every(isChecked);
+        const pin = pins[i];
+        const meetHousingTypeCondition = (housingType) => housingType.value === `any` || pin.offer.type === housingType.value;
+        const meetPriceCondition = (price) => {
+          switch (price.value) {
+            case `any`:
+              return true;
+            case `low`:
+              return pin.offer.price <= PRICE_FILTER_LOW;
+            case `middle`:
+              return pin.offer.price > PRICE_FILTER_LOW && pin.offer.price <= PRICE_FILTER_MIDDLE;
+            case `high`:
+              return pin.offer.price >= PRICE_FILTER_MIDDLE;
+            default:
+              return false;
+          }
+        };
+        const meetRoomsNumberCondition = (roomsNumber) => roomsNumber.value === `any` || pin.offer.rooms === Number(roomsNumber.value);
+        const meetGuestsNumberCondition = (guestsNumber) => guestsNumber.value === `any` || pin.offer.guests === Number(guestsNumber.value);
+        const meetFeaturesCondition = (features) => {
+          const checkFeature = (input) => !input.checked || (input.checked && pin.offer.features.includes(input.value));
+          return features.every(checkFeature);
+        };
 
-        const filterConditions = [housingTypeFilterConditions, priceFilterConditions, roomsNumberConditions, guestsNumberConditions, featuresConditions];
+        const meetConditions = meetHousingTypeCondition(elements.housingTypeFilter) && meetPriceCondition(elements.priceFilter) && meetRoomsNumberCondition(elements.roomsNumberFilter) && meetGuestsNumberCondition(elements.guestsNumberFilter) && meetFeaturesCondition(featuresInputsArray);
 
-        if (filterConditions.indexOf(false) === -1) {
-          filteredPins.push(pins[i]);
+        if (meetConditions) {
+          filteredPins.push(pin);
         }
         if (filteredPins.length === MAX_NUMBER_OF_PINS) {
           break;
